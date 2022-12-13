fca478fffdea51cf67030ca79eba248e49c42298
Danai <danai.dk@gmail.com>
2022-12-13 16:10:16 +0700

    Site ready to go

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,47 @@
 'use strict';
 
 const { src, dest, watch, series, parallel } = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
+// const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss')
 const cleanCSS = require('gulp-clean-css');
+const concat = require('gulp-concat');
 const sourcemaps = require('gulp-sourcemaps');
 const imagemin = require('gulp-imagemin');
+const ghpages = require('gh-pages');
 
 const browserSync = require('browser-sync').create();
 
-function scss() {
-  return src('src/css/app.scss')
+// function scss() {
+//   return src('src/css/app.scss')
+//     .pipe(sourcemaps.init())
+//     .pipe(sass.sync().on('error', sass.logError))
+//     .pipe(
+//       cleanCSS({
+//         compatibility: 'ie8'
+//       })
+//     )
+//     .pipe(sourcemaps.write('.'))
+//     .pipe(dest('dist'))
+//     .pipe(browserSync.stream());
+// };
+
+function css() {
+  return src([
+    "src/css/reset.css",
+    "src/css/typography.css",
+    "src/css/app.css"
+  ])
     .pipe(sourcemaps.init())
-    .pipe(sass.sync().on('error', sass.logError))
+    .pipe( 
+      postcss([ 
+        require('autoprefixer'), 
+        require('postcss-preset-env')({
+          stage: 1,
+          browsers: ['IE 11', 'last 2 versions']
+        }) 
+      ]) 
+    )
+    .pipe(concat('app.css'))
     .pipe(
       cleanCSS({
         compatibility: 'ie8'
@@ -46,12 +76,20 @@ function watchFiles() {
   });
 
   watch('src/*.html', html).on('change', browserSync.reload);
-  watch('src/css/app.scss', scss);
+  // watch('src/css/app.scss', scss);
+  watch('src/css/*.css', css);
   watch('src/fonts/*', fonts);
   watch('src/img/*', images);
 }
 
+function deploy(done) {
+  ghpages.publish('dist', function(err) {});
+  done();
+};
+
+exports.deploy = deploy;
+
 exports.default = series(
-  parallel(html, scss, fonts, images),
+  parallel(html, css, fonts, images),
   watchFiles
 );
